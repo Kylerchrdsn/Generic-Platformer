baac1b2c63c76445a128b5f90245723baa81d700
Kyle Richardson <krichardson@cd-1488.cdops.com>
2012-06-26 14:32:52 -0500

    moving some things around

--- main.cpp
+++ main.cpp
@@ -2,24 +2,19 @@
 #include "TheGame.h"
 
 
-int main(int argc, char* args[])
-{
+int main(int argc, char* args[]){
     TheGame controller;
     enum gameStates {INTRO, SELECT_SAVE_FILE, LEVEL_I, MENU, EXIT};
     controller.setGameState(INTRO);
 
-    if(!controller.initialize())
-    {
+    if(!controller.initialize()){
         return 1;
     }
 
-    while(controller.getGameState() != EXIT)
-    {
-        switch(controller.getGameState())
-        {
+    while(controller.getGameState() != EXIT){
+        switch(controller.getGameState()){
             case INTRO:
-                if(!controller.doIntro())
-                {
+                if(!controller.doIntro()){
                     return 1;
                 }
             break;
@@ -29,8 +24,7 @@ int main(int argc, char* args[])
             break;
 
             case LEVEL_I:
-                if(!controller.doLevelI())
-                {
+                if(!controller.doLevelI()){
                     return 1;
                 }
             break;
@@ -39,7 +33,6 @@ int main(int argc, char* args[])
                 controller.doMenu();
             break;
         };
-
     }
 
     controller.doExit();
